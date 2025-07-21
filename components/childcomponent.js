c04f2b6af469c758d2672045723d02724d9d9720
Jeshna J V <146533692+Jeshna11@users.noreply.github.com>
2025-07-21 15:46:00 +0530

    Delete components directory

--- components/childcomponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function ChildComponent(props) {
-  return (
-    <div>
-      <h2>Message from Parent: {props.message}</h2>
-    </div>
-  );
-}
-export default ChildComponent;
